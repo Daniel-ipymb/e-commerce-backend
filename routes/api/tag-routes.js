8b4d325c66fc1b77eee884419488cf153aade041
Daniel O <danielogunlana23@gmail.com>
2021-06-29 06:01:52 -0400

    updated delete route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -44,12 +44,40 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!tagData[0]) {
+      res.status(404).json({ message: "This tag doesn't exist!" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
-
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    if (!deleteTag) {
+      res.status(404).json({ message: "No tag exists with the id"});
+      return;
+    }
+    res.status(200).json(deleteTag);
+  } catch (error) {
+    res.status(500).json(error)
+    
+  }
 });
 
 module.exports = router;
