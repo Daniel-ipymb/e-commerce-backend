14734453ef3cb13fcd6e8847413c4f990625b625
Daniel O <danielogunlana23@gmail.com>
2021-06-29 04:53:21 -0400

    added product route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -59,8 +59,23 @@ router.put('/:id', async (req, res) => {
 
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    if (!deleteData) {
+      res.status(404).json({ message: "No category exists with the id"});
+      return;
+    }
+    res.status(200).json(deleteData);
+  } catch (error) {
+    res.status(500).json(error)
+    
+  }
 });
 
 module.exports = router;
