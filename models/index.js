d397346a2256764b45af6a034a494d325ff9aee9
Daniel O <danielogunlana23@gmail.com>
2021-07-29 03:03:02 -0400

    fixed tags

--- models/index.js
+++ models/index.js
@@ -17,15 +17,15 @@ Category.hasMany(Product, {
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
-    unique: false
-  },
+  },as:"products_tags",
+  foreignKey: "product_id"
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
-    unique: false
-  },
+  },as:'tags_products',
+  foreignKey: 'tag_id'
 })
 module.exports = {
   Product,
